91c0bad9c56c89408599275098d82b7cd5fe0daa
Dhanvarshini <dhanvarshini.k@numentica-ui.com>
2023-10-24 14:36:18 +0530

    changes in numberstostring

--- numberToString.js
+++ numberToString.js
@@ -7,6 +7,9 @@
 function convertNumberToString(number) {
     const digitNames = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
     const numberString = number.toString();
+    if(typeof number !== "number"){
+      return "invalid value";
+    }
     let result = "";
   
     for (let i = 0; i < numberString.length; i++) {
@@ -18,3 +21,5 @@ function convertNumberToString(number) {
   
   console.log(convertNumberToString(1));  
   console.log(convertNumberToString(98));
+  console.log(convertNumberToString("hello"));
+  console.log(convertNumberToString(true));
